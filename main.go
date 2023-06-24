2d0400f0561a5ad855b71a29aa0ffc8face7da19
RafaelClaumann <rafael_claumann@hotmail.com>
2023-06-24 13:31:12 -0300

    lendo variaveis do arquivo .env
    usando a lib 'github.com/joho/godotenv' para ler as variaveis do arquivo
    '.env'

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	envVarValue := os.Getenv("TESTE")
+	log.Print(envVarValue)
 
 }
