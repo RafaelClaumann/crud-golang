7a7086a8987ff6e6ef79e09b1bf686aadd2f4284
RafaelClaumann <rafael_claumann@hotmail.com>
2023-06-26 17:45:32 -0300

    implementacao simples dos handlers - user_controller
    https://github.com/gin-gonic/gin/blob/v1.9.1/docs/doc.md

--- src/controller/user_controller.go
+++ src/controller/user_controller.go
@@ -1,23 +1,41 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetUserById(c *gin.Context) {
+	userId := c.Param("userId")
 
+	c.JSON(http.StatusOK, gin.H{
+		"GetUserById": userId,
+	})
 }
 
 func GetUserByEmail(c *gin.Context) {
+	userEmail := c.Param("userEmail")
 
+	c.JSON(http.StatusOK, gin.H{
+		"GetUserByEmail": userEmail,
+	})
 }
 
 func CreateUser(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"CreateUser": "create-user",
+	})
 }
 
 func UpdateUser(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"UpdateUser": "update-user",
+	})
 }
 
 func DeleteUser(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"DeleteUser": "delete-user",
+	})
 }
