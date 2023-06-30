1ae950c591e2c9313b1c80c4cc9af8cc4da770f4
RafaelClaumann <rafael_claumann@hotmail.com>
2023-06-30 08:52:00 -0300

    validação dos campos no RequestBody
    a lib 'go-playground/validator' ja é importada de maneira indireta pelo
    gin-gonic.
    
    validator: `json:"email" validate:"required,email"`
    gin-gonic: `json:"email" binding:"required,email"`
    
    https://github.com/go-playground/validator
    https://pkg.go.dev/github.com/go-playground/validator
    
    https://pkg.go.dev/github.com/gin-gonic/gin#section-readme
    https://blog.logrocket.com/gin-binding-in-go-a-tutorial-with-examples/
    https://gin-gonic.com/docs/examples/binding-and-validation/

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Age      int    `json:"age" binding:"required,numeric,gte=18,lte=150"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%&*"`
 }
